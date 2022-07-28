331024ae8afb6d077c1282ddf0753fd36adb7c1b
Christian Munger <christianmngr2@gmail.com>
2022-07-28 15:17:16 -0400

    scala programming basics

--- src/main/scala/part4/pattern/matching/AllThePatterns.scala
+++ src/main/scala/part4/pattern/matching/AllThePatterns.scala
@@ -1,6 +1,73 @@
 package part4.pattern.matching
 
+import part2oop.Generics.MyList
+
+import scala.collection.View.Empty
+
 object AllThePatterns extends App {
+
+
+  // 1 - constants
+  val x: Any = "Scala"
+  val constants = x match {
+    case 1 => "a number"
+    case "scala" => "The scala"
+    case true => "The Truth"
+    case AllThePatterns => "a singleton object"
+  }
+
+  // 2 - match anything
+  // 2.1 - wildcard
+  val matchAnything = x match {
+    case _ =>
+  }
+
+  // 2.2 variable
+  val matchAVariable = x match {
+    case something => s"I've found $something"
+  }
+
+  // 3. Tuples
+  val aTuple = (1,2)
+  val matchATuple = aTuple match {
+    case (1,1) =>
+    case (something, 2) => s"I've found $something"
+  }
+
+  val nestedTuple = (1,(2,3))
+  val matchANestedTuple = nestedTuple match {
+    case (_, (2, v)) =>
+  }
+  // PMs can be nested
+
+  // 4. case classes - constructor pattern
+
+  // 5. List Patters
+  val aStandardList = List(1,2,3,42)
+  val standardListMatching = aStandardList match {
+    case List(1, _, _, _) => // extractor
+    case List(1, _*) => // List of arbitrary length
+    case 1 :: List(_) => // infix pattern
+    case List(1,2,3) :+ 42 => // infix pattern
+  }
+
+  // 6. Type specifiers
+  val unknown: Any = 2
+  val unknownMatch = unknown match {
+    case list: List[Int] => // explicit
+    case _ =>
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
